ceb8e78383730c3230d39ddf3939634d45706069
Ryan Karuna <ryan.karuna@gmail.com>
2026-03-11 20:11:29 +0700

    save local changes

---
+++ index.py
@@ -0,0 +1,12 @@
+from google import genai
+
+client = genai.Client()
+asd = 123
+
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
